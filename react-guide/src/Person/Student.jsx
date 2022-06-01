06c89f1cc28d85750647318461481712a9bca45b
Uzair Afridi <uzairafridi00@gmail.com>
2022-06-01 17:28:53 +0500

    Props children studied and applied

--- react-guide/src/Person/Student.jsx
+++ react-guide/src/Person/Student.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React from "react";
 
 function Student(props) {
   return (
-      <>
+    <>
       {/* Now we will Use React props (properties) to pass data dynamically */}
-      <h4>I am {props.name}, student of {props.school} and I am {props.age} years old.</h4>
-      </>
-  )
+      <h4>
+        I am {props.name}, student of {props.school} and I am {props.age} years
+        old.
+      </h4>
+      {/* props.children render all the things which is wrap
+       between Student component tag in App.js file i.e between closing and opening tag*/}
+      <p>{props.children}</p>
+    </>
+  );
 }
 
-export default Student
+export default Student;
