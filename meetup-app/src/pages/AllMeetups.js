b96dd73a61581b83ca1fc84262c7781b02dd4f59
Uzair Afridi <uzairafridi00@gmail.com>
2022-06-04 08:37:48 -0700

    Some changes in index CSS file

--- meetup-app/src/pages/AllMeetups.js
+++ meetup-app/src/pages/AllMeetups.js
@@ -3,6 +3,7 @@ import React from "react";
 function AllMeetups() {
   const DUMMY_DATA = [
     {
+<<<<<<< Updated upstream
       id: 'm1',
       title: 'This is a first meetup',
       image:
@@ -19,6 +20,24 @@ function AllMeetups() {
       address: 'Meetupstreet 5, 12345 Meetup City',
       description:
         'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+=======
+      id: "m1",
+      title: "This is a first meetup",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+      address: "Meetupstreet 5, 12345 Meetup City",
+      description:
+        "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+    },
+    {
+      id: "m2",
+      title: "This is a second meetup",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+      address: "Meetupstreet 5, 12345 Meetup City",
+      description:
+        "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+>>>>>>> Stashed changes
     },
   ];
   return (
