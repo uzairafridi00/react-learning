4c9be38f8e00f45fd00da0a6f332b200c7a0fd5b
Uzair Afridi <uzairafridi00@gmail.com>
2022-06-07 22:00:20 +0500

    Update Favourites.js

--- meetup-app/src/pages/Favourites.js
+++ meetup-app/src/pages/Favourites.js
@@ -7,10 +7,18 @@ import MeetupList from "../components/meetups/MeetupList";
 function Favourites() {
   const favouriteCtx = useContext(FavouritesContext);
 
+  let content;
+
+  if (favouriteCtx.totalFavourites === 0) {
+    content = <p>You got no favourites yet. Start adding some?</p>;
+  } else {
+    content = <MeetupList meetups={favouriteCtx.favourites} />;
+  }
+
   return (
     <div>
       <h1>My Favourites</h1>
-      <MeetupList meetups={favouriteCtx.favourites} />
+      {content}
     </div>
   );
 }
