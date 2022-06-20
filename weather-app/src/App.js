3dbbebd0dafde7483d0ce80d52429c5ddaa52edc
Uzair Afridi <uzairafridi00@gmail.com>
2022-06-20 20:21:05 +0500

    Update App.js

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,11 +1,23 @@
-import "./App.css";
+import './App.css';
+import React, { useEffect, useState } from "react";
+export default function App() {
 
-function App() {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+
+    console.log("Latitude is:", lat)
+    console.log("Longitude is:", long)
+  }, [lat, long]);
+
   return (
-    <>
-      <div className="App"><h1>Hello</h1></div>
-    </>
+    <div className="App">
+
+    </div>
   );
 }
-
-export default App;
