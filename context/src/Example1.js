536058441da30f4f6223488bf49e77f0316d9269
Uzair Afridi <uzairafridi00@gmail.com>
2022-06-21 22:29:08 +0500

    UseContext Created

---
+++ context/src/Example1.js
@@ -0,0 +1,29 @@
+import React, {useState} from 'react'
+
+export const Parent= () => {
+    const [userName, setUserName] = useState("Uzair");
+    return (
+        <div>
+            {userName}
+            <Child setUserName={setUserName} />
+        </div>
+    )
+};
+
+export const Child = (props) => {
+    return (
+        <GrandChild setUserName={props.setUserName} />
+    )
+};
+
+export const GrandChild = (props) => {
+    return (
+    <div>
+        <button onClick={() => {
+            setUserName("Uzair Afridi")
+        }}>
+
+        </button>
+    </div>
+)
+};
