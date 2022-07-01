2ea5a9f808e03f2ea43c19733d1d99a51491c919
Uzair Afridi <uzairafridi00@gmail.com>
2022-07-01 15:24:24 +0500

    Navbar Created

--- uzairafridi/src/App.js
+++ uzairafridi/src/App.js
@@ -1,9 +1,25 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home"
+import Projects from "./pages/Projects"
+import Experiences from "./pages/Experiences"
+
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-    
+      <div className="App">
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/projects" element={<Projects />}></Route>
+            <Route path="/experiences" element={<Experiences />}></Route>
+          </Routes>
+        </Router>
+      </div>
     </>
   );
 }
--- uzairafridi/tailwind.config.js
+++ uzairafridi/tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", 'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'],
-  plugins: [require("flowbite/plugin")],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: "class",
   theme: {
     extend: {},
