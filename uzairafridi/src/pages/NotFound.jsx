ffe685ebb8ffa008ed144db6ec7dcac314ca53ef
Uzair Afridi <uzairafridi00@gmail.com>
2022-07-05 12:14:33 +0500

    Update NotFound.jsx

--- uzairafridi/src/pages/NotFound.jsx
+++ uzairafridi/src/pages/NotFound.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function NotFound() {
   return (
@@ -11,15 +11,19 @@ function NotFound() {
         <div className="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">
           Page Not Found
         </div>
-        <button className="mt-5">
-          <a className="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring">
-            <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"></span>
 
-            <span className="relative block px-8 py-3 bg-[#1A2238] border border-current">
-              <Link to="/">Go Home</Link>
-            </span>
-          </a>
-        </button>
+        <Link to="/">
+          <button className="mt-5">
+            <a className="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring">
+              <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"></span>
+
+              <span className="relative block px-8 py-3 bg-[#1A2238] border border-current">
+                Go Home
+              </span>
+            </a>
+          </button>
+        </Link>
+        
       </main>
     </>
   );
