c3a60d2205f92e4e6294afe0bb92b3daccb05f8c
Uzair Afridi <uzairafridi00@gmail.com>
2022-07-06 22:11:29 +0500

    Code has been Optimized in Skills.jsx file

--- uzairafridi/src/pages/Skills.jsx
+++ uzairafridi/src/pages/Skills.jsx
@@ -1,60 +1,86 @@
-import React from 'react';
+import React from "react";
 
-import HTML from '../assets/images/html.png';
-import CSS from '../assets/images/css.png';
-import JavaScript from '../assets/images/javascript.png';
-import ReactImg from '../assets/images/react.png';
-import Node from '../assets/images/node.png';
-import FireBase from '../assets/images/firebase.png';
-import AWS from '../assets/images/aws.png';
-import GitHub from '../assets/images/github.png';
-import Tailwind from '../assets/images/tailwind.png';
-import Mongo from '../assets/images/mongo.png';
+import HTML from "../assets/images/html.png";
+import CSS from "../assets/images/css.png";
+import JavaScript from "../assets/images/javascript.png";
+import ReactImg from "../assets/images/react.png";
+import Node from "../assets/images/node.png";
+import FireBase from "../assets/images/firebase.png";
+import AWS from "../assets/images/aws.png";
+import GitHub from "../assets/images/github.png";
+import Tailwind from "../assets/images/tailwind.png";
+import Mongo from "../assets/images/mongo.png";
 
 const Skills = () => {
-  return (
-    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
-      {/* Container */}
-      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full' data-aos="fade-right">
-          <div>
-              <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
-              <p className='py-4'>These are the technologies I've worked with</p>
-          </div>
+  const skills = [
+    {
+      id: 1,
+      src: HTML,
+    },
+    {
+      id: 2,
+      src: CSS,
+    },
+    {
+      id: 3,
+      src: JavaScript,
+    },
+    {
+      id: 4,
+      src: ReactImg,
+    },
+    {
+      id: 5,
+      src: Node,
+    },
+    {
+      id: 6,
+      src: FireBase,
+    },
+    {
+      id: 7,
+      src: AWS,
+    },
+    {
+      id: 8,
+      src: GitHub,
+    },
+    {
+      id: 9,
+      src: Tailwind,
+    },
+    {
+      id: 10,
+      src: Mongo,
+    },
+  ];
 
-          <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-              <div className='cursor-pointer shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
-                  <p className='my-4'>HTML</p>
-              </div>
-              <div className='cursor-pointer shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={CSS} alt="HTML icon" />
-                  <p className='my-4'>CSS</p>
-              </div>
-              <div className='cursor-pointer shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={JavaScript} alt="HTML icon" />
-                  <p className='my-4'>JAVASCRIPT</p>
-              </div>
-              <div className='cursor-pointer shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={ReactImg} alt="HTML icon" />
-                  <p className='my-4'>REACT</p>
-              </div>
-              <div className='cursor-pointer shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={GitHub} alt="HTML icon" />
-                  <p className='my-4'>GITHUB</p>
-              </div>
-              <div className='cursor-pointer shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Node} alt="HTML icon" />
-                  <p className='my-4'>NODE JS</p>
-              </div>
-              <div className='cursor-pointer shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Mongo} alt="HTML icon" />
-                  <p className='my-4'>MONGO DB</p>
-              </div>
-              <div className='cursor-pointer shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={AWS} alt="HTML icon" />
-                  <p className='my-4'>AWS</p>
-              </div>
-          </div>
+  return (
+    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
+      {/* Container */}
+      <div
+        className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full"
+        data-aos="fade-right"
+      >
+        <div>
+          <p className="text-4xl font-bold inline border-b-4 border-pink-600 ">
+            Skills
+          </p>
+          <p className="py-4">These are the technologies I've worked with</p>
+        </div>
+
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+            {/* Parse the JSON from above data */}
+          {skills.map(({ id, src }) => (
+            <div
+              key={id}
+              className="cursor-pointer shadow-md shadow-[#040c16] hover:scale-110 duration-500"
+            >
+              <img className="w-20 mx-auto" src={src} alt={src} />
+              <p className="my-4">HTML</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
