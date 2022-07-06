30d06c4784411f6044dcd5bce89e6da731756471
Uzair Afridi <uzairafridi00@gmail.com>
2022-07-06 14:34:12 +0500

    First Example to use TS in react

---
+++ TYPE-SCRIPT/ts-react/src/App.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import './App.css';
+
+function App() {
+
+  const user = [
+    {name: "Ali", school: {name: "PMS School", location:"Gulbahar"}},
+    {name: "Umair", school: {name: "Forward School", location:"Dalazak"}},
+    {name: "Ahmad", school: null},
+  ]
+
+  return (
+    <>
+    <div className="App">
+      {user.map((user)=> {
+        return (
+          <div>
+            <h1>Name: {user.name}</h1>
+            <h3>School: {user.school?.name} {user.school?.location}</h3>
+          </div>
+        )
+      })}
+    </div>
+    </>
+  );
+}
+
+export default App;
