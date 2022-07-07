fb285f366370adbb2bbc31fd8cf4ed5b92a4c1d6
Uzair Afridi <uzairafridi00@gmail.com>
2022-07-07 21:39:29 +0500

    Enum have been learnt and Implemented

--- TYPE-SCRIPT/ts-course/src/App.tsx
+++ TYPE-SCRIPT/ts-course/src/App.tsx
@@ -1,5 +1,6 @@
 import React, {FC} from "react";
 import "./App.css";
+import { EnumAndType, HairColor } from "./components/EnumAndType";
 import { Person } from "./components/Person";
 import { PersonA } from "./components/PersonA";
 
@@ -8,7 +9,8 @@ const App: FC = () => {
     <>
       <div>Hello TypeScript App</div>
       {/* <Person name="Uzair" age={22} email="uzair@gmail.com"/> */}
-      <PersonA name="Uzair" age={22} email="uzair@gmail.com"/>
+      {/* <PersonA name="Uzair" age={22} email="uzair@gmail.com"/> */}
+      <EnumAndType  name="Uzair" age={22} email="uzair@gmail.com" hairColor={HairColor.Blonde}/>
     </>
   );
 };
---
+++ TYPE-SCRIPT/ts-course/src/components/EnumAndType.tsx
@@ -0,0 +1,40 @@
+import React, { FC, ChangeEvent, useState } from "react";
+
+export enum HairColor {
+  Blonde = "Hair is Blonde",
+  Brown = "Hair is Brown",
+  Pink = "Hair is Pink",
+  Black = "Hais is Black",
+}
+
+interface PersonProp {
+  name: string;
+  age: number;
+  email: string;
+  hairColor: HairColor,
+}
+
+export const EnumAndType: FC<PersonProp> = ({ name, age, email, hairColor }) => {
+  const [country, setCountry] = useState<string | null>("");
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setCountry(event.target.value);
+  };
+
+  return (
+    <>
+      <div>
+        <h1>{name}</h1>
+        <h1>{age}</h1>
+        <h1>{email}</h1>
+        <input
+          type="text"
+          placeholder="Write Down..."
+          onChange={handleChange}
+        />
+        <h1>{country}</h1>
+        <h2>{hairColor}</h2>
+      </div>
+    </>
+  );
+};
