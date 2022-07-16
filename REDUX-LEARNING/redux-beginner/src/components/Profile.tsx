ffd59107db810269593b699b39c0da99c4c10b3d
Uzair Afridi <uzairafridi00@gmail.com>
2022-07-16 22:26:32 +0500

    Update Profile.tsx

--- REDUX-LEARNING/redux-beginner/src/components/Profile.tsx
+++ REDUX-LEARNING/redux-beginner/src/components/Profile.tsx
@@ -6,7 +6,10 @@ export interface ProfileProp {
 export const Profile: FC<ProfileProp> = ({}) => {
   return (
     <div>
-      
+      <h1>Profile Page</h1>
+      <p>Name:</p>
+      <p>Age:</p>
+      <p>Email:</p>
     </div>
   );
 }
