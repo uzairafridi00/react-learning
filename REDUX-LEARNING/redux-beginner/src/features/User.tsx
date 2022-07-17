755a611d39ad9cf8f539b0e2a5d36a290a81b247
Uzair Afridi <uzairafridi00@gmail.com>
2022-07-17 09:16:40 +0500

    Update User.tsx

--- REDUX-LEARNING/redux-beginner/src/features/User.tsx
+++ REDUX-LEARNING/redux-beginner/src/features/User.tsx
@@ -1,12 +1,8 @@
-import React, {FC} from 'react';
+import React, { FC } from "react";
+import { createSlice } from "@reduxjs/toolkit";
 
-export interface UserProp {
-}
+export interface UserProp {}
 
 export const User: FC<UserProp> = ({}) => {
-  return (
-    <div>
-      
-    </div>
-  );
-}
+  return <div></div>;
+};
