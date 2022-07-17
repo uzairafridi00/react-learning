44a094a3bc5f483f80069e7bbb1cd49277610302
Uzair Afridi <uzairafridi00@gmail.com>
2022-07-17 08:33:39 +0500

    Store added to index file

--- REDUX-LEARNING/redux-beginner/src/index.tsx
+++ REDUX-LEARNING/redux-beginner/src/index.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import {configureStore} from "@reduxjs/toolkit"
+
+
+const store = configureStore({
+  reducer: {}
+})
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
