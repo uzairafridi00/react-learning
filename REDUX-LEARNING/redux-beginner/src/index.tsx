ba9486c0966812526e31551372d5b5775497928b
Uzair Afridi <uzairafridi00@gmail.com>
2022-07-17 08:34:38 +0500

    Store passed to provider

--- REDUX-LEARNING/redux-beginner/src/index.tsx
+++ REDUX-LEARNING/redux-beginner/src/index.tsx
@@ -1,20 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-import {configureStore} from "@reduxjs/toolkit"
-
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 
 const store = configureStore({
-  reducer: {}
-})
+  reducer: {},
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
-
