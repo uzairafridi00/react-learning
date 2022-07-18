eb8f0f9f42ed527412da37a735b16a27af34b884
Uzair Afridi <uzairafridi00@gmail.com>
2022-07-18 14:29:25 +0500

    Update User.ts

--- REDUX-LEARNING/redux-beginner/src/features/User.ts
+++ REDUX-LEARNING/redux-beginner/src/features/User.ts
@@ -17,6 +17,6 @@ export const userSlice = createSlice({
     }
 })
 
-export const {login} = userSlice.actions;
+export const {login, logout} = userSlice.actions;
 
 export default userSlice.reducer;
