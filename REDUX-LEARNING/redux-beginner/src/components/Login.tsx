136fe795d5ea28f40749d25eebe577de5ca7a819
Uzair Afridi <uzairafridi00@gmail.com>
2022-07-18 09:19:33 +0500

    Login profile updated with dispatch hook

--- REDUX-LEARNING/redux-beginner/src/components/Login.tsx
+++ REDUX-LEARNING/redux-beginner/src/components/Login.tsx
@@ -1,12 +1,20 @@
-import React, {FC} from 'react';
+import React, { FC } from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../features/User";
 
-export interface LoginProp {
-}
+export interface LoginProp {}
 
-export const Login:FC<LoginProp> = ({}) => {
+export const Login: FC<LoginProp> = ({}) => {
+  const dispatch = useDispatch();
   return (
     <div>
-      <button>Login</button>
+      <button
+        onClick={() => {
+          dispatch(login({ name: "Uzair", age: 22, email: "uzair@gmail.com" }));
+        }}
+      >
+        Login
+      </button>
     </div>
   );
-}
+};
--- REDUX-LEARNING/redux-beginner/src/components/Profile.tsx
+++ REDUX-LEARNING/redux-beginner/src/components/Profile.tsx
@@ -1,15 +1,19 @@
 import React, {FC} from 'react';
+import { useSelector } from 'react-redux';
 
 export interface ProfileProp {
 }
 
 export const Profile: FC<ProfileProp> = ({}) => {
+
+  const user = useSelector((state)=> state.user.value)
+
   return (
     <div>
       <h1>Profile Page</h1>
-      <p>Name:</p>
-      <p>Age:</p>
-      <p>Email:</p>
+      <p>Name: {user.name}</p>
+      <p>Age: {user.age}</p>
+      <p>Email: {user.email}</p>
     </div>
   );
 }
