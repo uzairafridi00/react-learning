6230153ab6f00e40dca9700f15d1676b0b555c4b
Uzair Afridi <uzairafridi00@gmail.com>
2022-07-18 14:44:51 +0500

    Update ChangeColor.tsx

--- REDUX-LEARNING/redux-beginner/src/components/ChangeColor.tsx
+++ REDUX-LEARNING/redux-beginner/src/components/ChangeColor.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from "react";
 
-export function ChangeColor () {
+export function ChangeColor() {
+  const [color, setColor] = useState<string | null>("");
+
   return (
     <div>
-      
+      <input
+        type="text"
+        onChange={(event) => {
+          setColor(event.target.value);
+        }}
+      />
+      <button>CHANGE COLOR</button>
     </div>
   );
 }
