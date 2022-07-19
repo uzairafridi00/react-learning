caf1158619124d6a9df2aa4768e1e2f6c7dd18ee
Uzair Afridi <uzairafridi00@gmail.com>
2022-07-19 08:48:20 +0500

    themeColor applied on app

--- REDUX-LEARNING/redux-beginner/src/App.tsx
+++ REDUX-LEARNING/redux-beginner/src/App.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import './App.css';
-import { ChangeColor } from './components/ChangeColor';
+import { useSelector } from 'react-redux';
 
+import { ChangeColor } from './components/ChangeColor';
 import { Login } from './components/Login';
 import { Profile } from './components/Profile';
 
 function App() {
+
+  const themeColor = useSelector<any>((state)=> state.theme.value);
+
   return (
-    <div>
+    // <div style={{backgroundColor:themeColor}}>
       <div className='App'>
       <Profile />
       <Login />
       <ChangeColor />
     </div>
-    </div>
+    // </div>
   );
 }
 
