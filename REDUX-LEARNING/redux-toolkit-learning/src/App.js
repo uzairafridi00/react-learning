a2cf341c946f9d88043d382c2dfc06dc8658a658
Uzair Afridi <uzairafridi00@gmail.com>
2022-07-27 12:38:16 +0500

    modalSlice has been created with Modal Component

--- REDUX-LEARNING/redux-toolkit-learning/src/App.js
+++ REDUX-LEARNING/redux-toolkit-learning/src/App.js
@@ -1,26 +1,26 @@
-import CartContainer from "./components/CartContainer";
-import Navbar from "./components/Navbar";
-
 import { useDispatch, useSelector } from "react-redux";
 import { calculateTotals } from "./features/cart/cartSlice";
 import { useEffect } from "react";
 
-function App() {
-  const { cartItems } = useSelector((store) => store.cart);
-  const dispatch = useDispatch();
+import CartContainer from "./components/CartContainer";
+import Navbar from "./components/Navbar";
+import Modal from "./components/Modal";
 
+function App() {
+  const { cartItems } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
   useEffect(() => {
     dispatch(calculateTotals());
+    // eslint-disable-line react-hooks/exhaustive-deps
   }, [cartItems]);
 
   return (
-    <>
-      <main>
-        <App />
-        <Navbar />
-        <CartContainer />
-      </main>
-    </>
+    <main>
+      <Modal />
+      <Navbar />
+      <CartContainer />
+    </main>
   );
 }
+
 export default App;
