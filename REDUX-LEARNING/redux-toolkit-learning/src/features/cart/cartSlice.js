099d1de94f4090b864b0a68f889c68e8275612bb
Uzair Afridi <uzairafridi00@gmail.com>
2022-07-27 11:41:40 +0500

    Increase and Decrease reducers created

--- REDUX-LEARNING/redux-toolkit-learning/src/features/cart/cartSlice.js
+++ REDUX-LEARNING/redux-toolkit-learning/src/features/cart/cartSlice.js
@@ -16,6 +16,15 @@ const cartSlice = createSlice({
     clearCart: (state) => {
       state.cartItems = [];
     },
+
+    increase: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount + 1;
+    },
+    decrease: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount - 1;
+    },
   },
 });
 
