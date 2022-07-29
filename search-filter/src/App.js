0724f8ecd93e1ecc9d76bef8ccf7b4e2528cc60c
Uzair Afridi <uzairafridi00@gmail.com>
2022-07-29 10:54:56 +0500

    Search App created

--- search-filter/src/App.js
+++ search-filter/src/App.js
@@ -1,10 +1,56 @@
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [searchTitle, setSearchTitle] = useState("");
+
+  useEffect(() => {
+    function getData() {
+      setLoading(true);
+      fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((res) => res.json())
+        .then((data) => {
+          setLoading(false);
+          setPosts(data);
+        })
+        .catch((err) => {
+          setLoading(false);
+          console.log(err);
+        });
+    }
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <div className="App">
+        <h1>Search Filter</h1>
+        <input
+          type="text"
+          placeholder="Search..."
+          onChange={(e) => setSearchTitle(e.target.value)}
+        />
+        {loading ? (
+          <h4>Loading...</h4>
+        ) : (
+          posts
+            .filter((value) => {
+              if (searchTitle === "") {
+                return value;
+              } else if (
+                value.title
+                  .toLowerCase()
+                  .includes(searchTitle.toLocaleLowerCase())
+              ) {
+                return value;
+              }
+            })
+            .map((item) => <h5 key={item.id}>{item.title}</h5>)
+        )}
+      </div>
+    </>
   );
 }
 
